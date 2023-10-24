81c49a12a109b5451d30e0dc2478c7dcaf246e20
KAN KATPARK <kant.cmd@gmail.com>
2023-10-24 21:08:29 +0700

    test 2

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import cProfile
+import glob
 import os
 import re
 import subprocess
 
+from pathlib import Path
 
 root = os.getcwd()
 print(root)
@@ -12,22 +14,27 @@ with open("./input.csv", 'r') as file:
     for line in file: 
         if len(line) == 0 : continue
         token = line.strip().split(",")
-    # lines = file.read().strip().split("\n")
+    lines = file.read().strip().split("\n")
 
-    # for line in lines:
+    for line in lines:
         
         repo_url = token[2].strip()
         project_dir = os.path.join(root, f"tmp/Project-{n}")
-        os.makedirs(project_dir, exist_ok=True)
+        # os.makedirs(project_dir, exist_ok=True)
         
         # Clone the Git repository
-        subprocess.run(["git", "clone", repo_url, project_dir])
+        # subprocess.run(["git", "clone", repo_url, project_dir])
         os.chdir(project_dir)
 
         file_list = os.listdir(project_dir)
-        py_files = [file for file in file_list if file.endswith('.py')]
+        
+        py_files = list(Path(".").rglob("*.py"))
 
-        for file in py_files:
-            print(file)
+        c_result = cProfile.run( os.path.join(project_dir, "*.py") )
+    
+        # for py_file in py_files:
+        #     command = f"python -m cProfile -o profile.stats {py_file}"
+        #     c_result = subprocess.run(command, shell=True)
+        #     print(c_result)
         
         n = n+1
